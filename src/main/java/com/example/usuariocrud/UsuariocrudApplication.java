f10d0d3f1d0a0699dabacdd0b06fbe63f8a0f2fa
marceloriv <ma.riveracontreras@duocuc.com>
2025-05-11 17:34:06 -0400

    Agregar archivos de configuración y estructura inicial del proyecto para el CRUD de usuarios

---
+++ src/main/java/com/example/usuariocrud/UsuariocrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.usuariocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariocrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/usuariocrud/UsuariocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usuariocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariocrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
