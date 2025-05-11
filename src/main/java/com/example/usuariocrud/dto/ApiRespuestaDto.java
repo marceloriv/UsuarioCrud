c1b2a94c9fac1d7f90f9fa7b86aabaf4d4602fd7
marceloriv <ma.riveracontreras@duocuc.com>
2025-05-11 17:57:36 -0400

    Agregar clase ApiRespuestaDto para estructurar respuestas de la API

---
+++ src/main/java/com/example/usuariocrud/dto/ApiRespuestaDto.java
@@ -0,0 +1,13 @@
+package com.example.usuariocrud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiRespuestaDto {
+
+    private String estado;
+    private String mensaje;
+
+}
