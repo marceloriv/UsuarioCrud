48e5709e8fbe2eddd87cc240fd323676fdd9ca58
marceloriv <ma.riveracontreras@duocuc.com>
2025-05-13 02:43:49 -0400

    Agregar excepción UsuarioNoEncontradoException para manejar errores de usuario no encontrado

---
+++ src/main/java/com/example/usuariocrud/exeception/UsuarioNoEncontradoException.java
@@ -0,0 +1,9 @@
+package com.example.usuariocrud.exeception;
+
+public class UsuarioNoEncontradoException extends Exception {
+
+    public UsuarioNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+
+}
